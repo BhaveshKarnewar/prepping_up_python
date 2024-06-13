c34727c72abae9bc83b87fd042df1e6dd96d5e45
BhaveshKarnewar <bhaveshkarnewar2@gmail.com>
2024-06-13 23:44:07 +0530

    add changes

--- 06_scopes/scopes.py
+++ 06_scopes/scopes.py
@@ -1,13 +1,13 @@
-def f1(para1,para2):
-  sum=para1+para2
-  def f2(para3):
-    return para3**sum
-  return f2
+# def f1(para1,para2):
+#   sum=para1+para2
+#   def f2(para3):
+#     return para3**sum
+#   return f2
 
-a= f1(2,3)
+# a= f1(2,3)
 
-print(a(5))
-print(5**5)
+# print(a(5))
+# print(5**5)
 
-
-
+str="matata"
+print(str.count("t"))
